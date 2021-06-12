74412b5b93cb577039e60c4555f398c43ddf9fe6
Oleg.Ivshin <Oleg.Ivshin@south.rt.ru>
2021-06-12 13:45:42 +0300

    hello

--- src/main/java/com/example0/Application.java
+++ src/main/java/com/example0/Application.java
@@ -2,7 +2,10 @@ package com.example0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
 
@@ -10,4 +13,10 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hallo";
+	}
+	
 }
